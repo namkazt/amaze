9e70b631b9e7a8232bc99796a1c01e6323403ef6
Nam Nguyễn <nam.kazt.91@gmail.com>
2022-02-24 15:11:37 +0700

    add text

--- js/amaze.js
+++ js/amaze.js
@@ -189,6 +189,32 @@ target.anchor.x = 0.5;
 target.anchor.y = 0.5;
 container.addChild(target);
 
+const styleLogo = new PIXI.TextStyle({
+  fontFamily: "Arial",
+  fontSize: 36,
+  fontStyle: "italic",
+  fontWeight: "bold",
+  fill: ["#ffffff", "#00ff99"], // gradient
+  stroke: "#4a1850",
+  strokeThickness: 5,
+  dropShadow: true,
+  dropShadowColor: "#000000",
+  dropShadowBlur: 4,
+  dropShadowAngle: Math.PI / 6,
+  dropShadowDistance: 6,
+  wordWrap: true,
+  wordWrapWidth: 1000,
+  lineJoin: "round",
+});
+const logoText = new PIXI.Text(
+  "Vanilla flavored tablet is Pleasant to Patient",
+  styleLogo
+);
+logoText.x = 1;
+logoText.y = 10;
+logoText.scale.set(1 / scale, 1 / scale);
+container.addChild(logoText);
+
 // init player
 let winPopup = new PIXI.Graphics();
 const style = new PIXI.TextStyle({
